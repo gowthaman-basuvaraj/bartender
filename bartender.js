857ee53c50ca583bbaaf80037770038708d7ddc7
Gowthaman B <gowthaman@365build.in>
2014-08-08 20:43:37 +0530

    use co-sleep add datejs and other libs, add a primitive text parsing for date/time

--- bartender.js
+++ bartender.js
@@ -1,160 +1,156 @@
 (function () {
-	"use strict";
-	
-	var Datastore = require('nedb'),	
-		fs = require("fs"),
-		co = require("co"),
-		TwitterAPI = require('./twitterapi');
+    "use strict";
 
-	var Bartender = function() {
-		var config = require("./config.json");
-		this.twitterAPI = new TwitterAPI(config.account, config.apiKey, config.apiSecret, config.accessToken, config.accessTokenSecret);
+    var Datastore = require('nedb'),
+        fs = require("fs"),
+        co = require("co"),
+        sleep = require('co-sleep'),
+        TwitterAPI = require('./twitterapi');
 
-		this.db = {
-			meetups: new Datastore('data/meetups.db'),
-			beermugs: new Datastore('data/beermugs.db'),
-			wisdom: new Datastore('data/wisdom.db')
-		};
+    var Bartender = function () {
+        var config = require("./config.json");
+        this.twitterAPI = new TwitterAPI(config.account, config.apiKey, config.apiSecret, config.accessToken, config.accessTokenSecret);
 
-		var sleep = function(ms) {
-			return function (cb) {
-				setTimeout(cb, ms);
-			};
-		}
-			
-		co(function*() {
-			while(true) {
-				try {
-					yield this.read();
-					yield sleep(60000);
-				} 
-				catch (ex) {
-					yield this.log(ex);
-				}
-			}
-		}).call(this);
-	};
+        this.db = {
+            meetups: new Datastore('data/meetups.db'),
+            beermugs: new Datastore('data/beermugs.db'),
+            wisdom: new Datastore('data/wisdom.db')
+        };
+
+
+        co(function*() {
+            while (true) {
+                try {
+                    yield this.read();
+                    yield sleep(60 * 1000);
+                }
+                catch (ex) {
+                    yield this.log(ex);
+                }
+            }
+        }).call(this);
+    };
 
 
     /*
      * Read @beercombinator's timeline
      */
-	Bartender.prototype.read = function*() {
-		var messages = yield this.twitterAPI.read();
-		for (var i = 0; i < messages.length; i++) {
-			yield this.process(messages[i]);
-		}
-	};
+    Bartender.prototype.read = function*() {
+        var messages = yield this.twitterAPI.read();
+        for (var i = 0; i < messages.length; i++) {
+            yield this.process(messages[i]);
+        }
+    };
 
 
     /*
      * Process messages
      */
-	Bartender.prototype.process = function*(message) {
-		var handler = {
-			"MEETUP": this.meetup,
-			"MEETUP_CANCEL": this.cancelMeetup,
-			"RSVP_YES": this.rsvpYes,
-			"RSVP_NO": this.rsvpNo,
-			"BEER_MUG": this.beerMug,
-			"WISDOM": this.wisdom
-		}[message.type];
+    Bartender.prototype.process = function*(message) {
+        var handler = {
+            "MEETUP": this.meetup,
+            "MEETUP_CANCEL": this.cancelMeetup,
+            "RSVP_YES": this.rsvpYes,
+            "RSVP_NO": this.rsvpNo,
+            "BEER_MUG": this.beerMug,
+            "WISDOM": this.wisdom
+        }[message.type];
 
-		if (handler) {
-			yield handler.call(this, message);
-		} else {
-			this.log("Cannot understand this message: " + JSON.stringify(message));
-		}
-	};
-
-	
-	/*
-	 * Schedule a meetup.
-	 *
-	 * Organizer should tweet this: 
-	 * @beercombinator 11AM Friday, Some Location #meetup
-	 *
-	 * Sender is assumed to be the meetup organizer.
-	 * @beercombinator will retweet to all followers.
-	 */	
-	Bartender.prototype.meetup = function*(message) {
-		yield this.twitterAPI.broadcast("");
-	};
+        if (handler) {
+            yield handler.call(this, message);
+        } else {
+            this.log("Cannot understand this message: " + JSON.stringify(message));
+        }
+    };
 
 
-	/*
-	 * Cancel a meetup
-	 *
-	 * Organizer should tweet this:
-	 * @beercombinator #cancel
-	 *
-	 * A previously scheduled meetup will be cancelled.
-	 * @beercombinator will retweet to followers.)
-	 */
-	Bartender.prototype.cancelMeetup = function*(message) {
-		yield this.twitterAPI.broadcast("");
-
-		var going = yield Meetup.find();
-		for (var i = 0; i < going.length; i++) {
-			yield this.twitterAPI.message()
-		}
-	};
-
-	
-	/*
-	 * RSVP Yes
-	 *
-	 * Tweet this to say YES
-	 * @beercombinator @organizer #yes
-	 */
-	Bartender.prototype.rsvpYes = function*(message) {
-		var meetup = yield Meetup.find();
-		yield this.twitterAPI.message()
-	};
+    /*
+     * Schedule a meetup.
+     *
+     * Organizer should tweet this:
+     * @beercombinator 11AM Friday, Some Location #meetup
+     *
+     * Sender is assumed to be the meetup organizer.
+     * @beercombinator will retweet to all followers.
+     */
+    Bartender.prototype.meetup = function*(message) {
+        yield this.twitterAPI.broadcast("");
+    };
 
 
-	/*
-	 * RSVP No
-	 *
-	 * Tweet this to cancel a previous YES
-	 * @beercombinator @organizer #no
-	 */
-	Bartender.prototype.rsvpNo = function*(message) {
-		var meetup = yield Meetup.find();
-		yield this.twitterAPI.message()
-	};
+    /*
+     * Cancel a meetup
+     *
+     * Organizer should tweet this:
+     * @beercombinator #cancel
+     *
+     * A previously scheduled meetup will be cancelled.
+     * @beercombinator will retweet to followers.)
+     */
+    Bartender.prototype.cancelMeetup = function*(message) {
+        yield this.twitterAPI.broadcast("");
+
+        var going = yield Meetup.find();
+        for (var i = 0; i < going.length; i++) {
+            yield this.twitterAPI.message()
+        }
+    };
 
 
-	/*
-	 * Offer a free mug of beer to 1 person
-	 *
-	 * Tweet this:
-	 * @beercombinator Some pub, 5PM Today #beermug
-	 */
-	Bartender.prototype.beerMug = function*() {
-		
-	};
+    /*
+     * RSVP Yes
+     *
+     * Tweet this to say YES
+     * @beercombinator @organizer #yes
+     */
+    Bartender.prototype.rsvpYes = function*(message) {
+        var meetup = yield Meetup.find();
+        yield this.twitterAPI.message()
+    };
 
 
-	/*
-	 * Get some wisdom from the bartender.
-	 *
-	 * Tweet this to get a reply back from @beercombinator
-	 * @beercombinator #wisdom
-	 */
-	Bartender.prototype.wisdom = function*() {
-		
-	};
+    /*
+     * RSVP No
+     *
+     * Tweet this to cancel a previous YES
+     * @beercombinator @organizer #no
+     */
+    Bartender.prototype.rsvpNo = function*(message) {
+        var meetup = yield Meetup.find();
+        yield this.twitterAPI.message()
+    };
 
 
-	Bartender.prototype.sendStats = function*() {
-		
-	};
+    /*
+     * Offer a free mug of beer to 1 person
+     *
+     * Tweet this:
+     * @beercombinator Some pub, 5PM Today #beermug
+     */
+    Bartender.prototype.beerMug = function*() {
+
+    };
 
 
-	Bartender.prototype.log = function*(message) {
-		console.log("log(" + Date().toString() + ") " + JSON.stringify(message));
-	};
+    /*
+     * Get some wisdom from the bartender.
+     *
+     * Tweet this to get a reply back from @beercombinator
+     * @beercombinator #wisdom
+     */
+    Bartender.prototype.wisdom = function*() {
 
-	module.exports = Bartender;
+    };
+
+
+    Bartender.prototype.sendStats = function*() {
+
+    };
+
+
+    Bartender.prototype.log = function*(message) {
+        console.log("log(" + Date().toString() + ") " + JSON.stringify(message));
+    };
+
+    module.exports = Bartender;
 })();
--- twitterapi.js
+++ twitterapi.js
@@ -1,136 +1,137 @@
-(function() {
-	"use strict";
+(function () {
+    "use strict";
 
-	var API = require("node-twitter-api");
-
-
-	var TwitterAPI = function(account, apiKey, apiSecret, accessToken, accessTokenSecret) {
-		this._api = new API({
-			consumerKey: apiKey,
-			consumerSecret: apiSecret,
-			callback: ""
-		});
-
-		this.account = account;
-		this.accessToken = accessToken;
-		this.accessTokenSecret = accessTokenSecret;
-	}
+    var API = require("node-twitter-api");
+    var date_js = require("datejs");
+    var mentions_regex = /^@[a-zA-Z0-9_]+$/;
+    var hash_regex = /^#[^ ]+&/;
 
 
-	/*
-	 * Thunkify it for co+ES6 generators
-	 */
-	TwitterAPI.prototype.api = function(namespace, name, params, token, secret) {
-		var self = this;
-		return function(cb) {
-			self._api[namespace](name, params, token, secret, function(error, data, response) {
-				cb(error, { data: data, response: response });
-			});
-		};
-	}
-	
+    var TwitterAPI = function (account, apiKey, apiSecret, accessToken, accessTokenSecret) {
+        this._api = new API({
+            consumerKey: apiKey,
+            consumerSecret: apiSecret,
+            callback: ""
+        });
 
-	/* 
-	 * Read tweets from the main account.
-	 * Called periodically to check for new tweets
-	 */
-	TwitterAPI.prototype.read = function*() {		
-		var tweets = yield this.api("getTimeline", "mentions_timeline", {}, this.accessToken, this.accessTokenSecret);
-
-		tweets.data.map(function(item) {
-			if (item.text.indexOf('#meetup') > 0)
-				return this.parseMeetup(item);
-			else if (item.text.indexOf('#cancel') > 0)
-				return this.parseCancelMeetup(item);
-			else if (item.text.indexOf('#yes') > 0)
-				return this.parseRsvpYes(item);
-	   		else if (item.text.indexOf('#no') > 0)
-				return this.parseRsvpNo(item);
-			else if (item.text.indexOf('#beermug') > 0)
-				return this.parseBeerMug(item);
- 			else if (item.text.indexOf('#wisdom') > 0)
-				return this.parseWisdom(item);
-		}, this);
-	}
-	
-	
-	TwitterAPI.prototype.parseMeetup = function(item) {
-		var result = this.parseBasic(item);
-		result.type = "MEETUP";
-		return result;
-	};
+        this.account = account;
+        this.accessToken = accessToken;
+        this.accessTokenSecret = accessTokenSecret;
+    }
 
 
-	TwitterAPI.prototype.parseCancelMeetup = function(item) {
-		var result = this.parseBasic(item);
-		result.type = "CANCEL_MEETUP";
-		return result;
-	};
+    /*
+     * Thunkify it for co+ES6 generators
+     */
+    TwitterAPI.prototype.api = function (namespace, name, params, token, secret) {
+        var self = this;
+        return function (cb) {
+            self._api[namespace](name, params, token, secret, function (error, data, response) {
+                cb(error, { data: data, response: response });
+            });
+        };
+    }
 
 
-	TwitterAPI.prototype.parseRsvpYes = function(item) {
-	    var result = {};
-		result.type = "RSVP_YES";
-	    result.user = this.parseUser(item);
+    /*
+     * Read tweets from the main account.
+     * Called periodically to check for new tweets
+     */
+    TwitterAPI.prototype.read = function*() {
+        var tweets = yield this.api("getTimeline", "mentions_timeline", {}, this.accessToken, this.accessTokenSecret);
 
-	    var mentions = this.parseMentions(item);	    
+        tweets.data.map(function (item) {
+            if (item.text.indexOf('#meetup') > 0)
+                return this.parseMeetup(item);
+            else if (item.text.indexOf('#cancel') > 0)
+                return this.parseCancelMeetup(item);
+            else if (item.text.indexOf('#yes') > 0)
+                return this.parseRsvpYes(item);
+            else if (item.text.indexOf('#no') > 0)
+                return this.parseRsvpNo(item);
+            else if (item.text.indexOf('#beermug') > 0)
+                return this.parseBeerMug(item);
+            else if (item.text.indexOf('#wisdom') > 0)
+                return this.parseWisdom(item);
+        }, this);
+    }
+
+
+    TwitterAPI.prototype.parseMeetup = function (item) {
+        var result = this.parseBasic(item);
+        result.type = "MEETUP";
+        return result;
+    };
+
+
+    TwitterAPI.prototype.parseCancelMeetup = function (item) {
+        var result = this.parseBasic(item);
+        result.type = "CANCEL_MEETUP";
+        return result;
+    };
+
+
+    TwitterAPI.prototype.parseRsvpYes = function (item) {
+        var result = {};
+        result.type = "RSVP_YES";
+        result.user = this.parseUser(item);
+
+        var mentions = this.parseMentions(item);
         if (mentions.length) {
-            
+
         } else {
             return this.parseError("You need to include the organizer. See these examples.");
         }
 
-		return result;
-	};
+        return result;
+    };
 
 
     /*
      * @beercombinator #no
      */
-	TwitterAPI.prototype.parseRsvpNo = function(item) {
-	    var result = {};
-		result.type = "RSVP_NO";
-	    result.user = this.parseUser(item);
+    TwitterAPI.prototype.parseRsvpNo = function (item) {
+        var result = {};
+        result.type = "RSVP_NO";
+        result.user = this.parseUser(item);
 
-	    var mentions = this.parseMentions(item);	    
+        var mentions = this.parseMentions(item);
         if (mentions.length) {
-            
+
         } else {
             return this.parseError("You need to include the organizer. See these examples.");
         }
 
-		return result;
-	};
+        return result;
+    };
 
 
     /*
      * @beercombinator 4PM Today, Tavern Church Street #beermug
      */
-	TwitterAPI.prototype.parseBeerMug = function(item) {
-	    var result = {};
-		result.type = "BEER_MUG";
-	    result.user = this.parseUser(item);
-	    result.time = this.parseTime(item);	
-		return result;
-	};
-
+    TwitterAPI.prototype.parseBeerMug = function (item) {
+        var result = {};
+        result.type = "BEER_MUG";
+        result.user = this.parseUser(item);
+        result.time = this.parseTime(item);
+        return result;
+    };
 
 
     /*
      * @beercombinator #widsom
      */
-	TwitterAPI.prototype.parseWisdom = function(item) {
-	    var result = {};
-		result.type = "WISDOM";
-	    result.user = this.parseUser(item);
-		return result;
-	};
-	
+    TwitterAPI.prototype.parseWisdom = function (item) {
+        var result = {};
+        result.type = "WISDOM";
+        result.user = this.parseUser(item);
+        return result;
+    };
 
 
-	TwitterAPI.prototype.parseUser = function(item) {
-	    return { id: item.user.id, username: item.user.screen_name, name: item.user.name };
-	}
+    TwitterAPI.prototype.parseUser = function (item) {
+        return { id: item.user.id, username: item.user.screen_name, name: item.user.name };
+    }
 
 
     /*
@@ -138,30 +139,75 @@
      * {10AM, 10 AM} ['', ',', 'on'] {Friday, Tomorrow, 2nd May, May 2nd, May 2, 2 May}
      * {Friday, Tomorrow, 2nd May, May 2nd, May 2, 2 May} ['', ',', 'at'] {10AM, 10 AM}
      */
-	TwitterAPI.prototype.parseTime = function(item) {
-		return {
-		    time: "",
-		    date: ""
-		}
-	}
+    TwitterAPI.prototype.parseTime = function (item) {
+        //date_js does not work with other arbitary stuff, may be we can tokenize and then
+        //try to parse parts of it
+
+        //say: @beer guys tommorrow 6pm #fun
+
+        //to optimize may be we can remove well known item, say @mentions #hashtags
+
+        //lets tokenize
+        var tokens = item.text.split(" "); //primitive, but for now lets tokenize by space
+
+        var filtered_tokens = tokens.filter(function (token) {
+            return token && token.length > 0 && !token.match(mentions_regex) && !token.match(hash_regex)
+        });
+
+        //filtered_tokens = [guys, tomorrow, 6pm]
+        //we'll construct strings
+
+        //0.0 guys tomorrow 6pm
+        //0.1 guys tomorrow
+        //0.2 guys
+
+        //1.0 tomorrow 6pm
+        //1.1 tomorrow
+
+        //2.0 6pm
+
+        //the first match we'll take it as datetime
+        var idx = 0, max_len = filtered_tokens.length,
+            idx_inner = 0;
+
+        for (idx = 0; idx < max_len; idx++) {
+            for (idx_inner = max_len; idx_inner > idx; idx_inner--) {
+
+                var new_text = filtered_tokens.slice(idx, idx_inner);
+
+                var parsed_date = date_js.parse(new_text);
+                if (parsed_date)
+                    return {
+                        time: parsed_date.toString("hh:mm tt"),
+                        date: parsed_date.toString("dd/MM/yyyy")
+                    }
+            }
+
+        }
+
+        return {
+            time: "",
+            date: ""
+        }
+    }
 
 
-	TwitterAPI.prototype.parseMentions = function(item) {
-		return [];
-	}
+    TwitterAPI.prototype.parseMentions = function (item) {
+        return [];
+    }
 
 
     /*
      * If the message was not sent to @beercombinator in the right format
      */
-	TwitterAPI.prototype.parseError = function(message) {
-		return {
-		    type: "ERROR",
-		    message: message
-		}
-	}
+    TwitterAPI.prototype.parseError = function (message) {
+        return {
+            type: "ERROR",
+            message: message
+        }
+    }
 
 
-	module.exports = TwitterAPI;
+    module.exports = TwitterAPI;
 
 })();
